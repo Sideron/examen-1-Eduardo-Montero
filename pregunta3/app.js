19ccae33d328817a1038a3a7c1f4e1718c3b83ff
O23427 <O23427@lab.aca>
2024-09-11 21:41:57 -0500

    a

--- pregunta3/app.js
+++ pregunta3/app.js
@@ -1,4 +1,5 @@
 const display = document.getElementById('elementos')
+const suma = document.getElementById('suma');
 let misProductos = []
 
 function Producto(nomb, cant, prec){
@@ -15,7 +16,7 @@ const addProducto = (nombre, cantidad,precio) => {
     misProductos.push(nProd);
 }
 
-addProducto("Papas", 3,2.5)
+addProducto("Papas", 4,2.5)
 addProducto("Camotes", 5,1.2)
 
 const updateTable = () => {
@@ -27,5 +28,12 @@ const updateTable = () => {
         <th>Cantidad</th>
         <th>Precio</th>
     </tr>`+elementos;
+    
+    let total = 0
+    const sumar = (num) => {
+        total += num
+    }
+    misProductos.forEach((x) => {sumar(x.precio*x.cantidad)})
+    suma.innerHTML = `Precio total es: ${total}`;
 }
 updateTable();
